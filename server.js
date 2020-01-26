854e563fe33569936456ac0797f7aebea7edaaf0
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-01-26 12:00:49 +0530

    deleted unnecessary statements

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ const { processRequest } = require('./lib/processRequest');
 
 const handleConnection = function(socket) {
   const remote = { address: socket.remoteAddress, port: socket.remotePort };
-  console.log('connected to >> ', remote);
   socket.on('data', text => {
-    console.log(text);
     const req = Request.parse(text);
     const res = processRequest(req);
-    console.log(res);
     res.writeTo(socket);
   });
+  socket.on('end', () => console.warn(remote, 'ended'));
   socket.setEncoding('utf8');
 };
 

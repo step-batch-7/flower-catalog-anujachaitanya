41ff98a6c3825e52dcd352a48071d555334140ee
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-01-26 11:52:14 +0530

    Added server, we can request flower catalog from server
    have to add vani feature to gif
    and have to add guest book

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { Server } = require('net');
 const Request = require('./lib/request');
+const { processRequest } = require('./lib/processRequest');
 
 const handleConnection = function(socket) {
   const remote = { address: socket.remoteAddress, port: socket.remotePort };
@@ -7,7 +8,9 @@ const handleConnection = function(socket) {
   socket.on('data', text => {
     console.log(text);
     const req = Request.parse(text);
-    console.log(req);
+    const res = processRequest(req);
+    console.log(res);
+    res.writeTo(socket);
   });
   socket.setEncoding('utf8');
 };

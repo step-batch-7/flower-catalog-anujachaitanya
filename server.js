b05775c8e4a30923639fa8e11520f613a881bcc6
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-01-28 14:55:26 +0530

    moved request to http Request

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
-const { Server } = require('net');
-const Request = require('./lib/request');
+const http = require('http');
 const { processRequest } = require('./lib/processRequest');
 
-const handleConnection = function(socket) {
-  const remote = { address: socket.remoteAddress, port: socket.remotePort };
-  socket.on('data', text => {
-    const req = Request.parse(text);
-    const res = processRequest(req);
-    res.writeTo(socket);
-  });
-  socket.on('end', () => console.warn(remote, 'ended'));
-  socket.setEncoding('utf8');
+const handleConnection = function(req, res) {
+  const responseText = processRequest(req);
+  res.end(responseText.body);
 };
 
 const main = function() {
-  const server = new Server();
-  server.on('connection', handleConnection);
-  server.listen(2300);
+  const server = new http.Server(handleConnection);
+  server.listen(2300, () => {
+    console.log('started listening on port :', server.address());
+  });
 };
 
 main();

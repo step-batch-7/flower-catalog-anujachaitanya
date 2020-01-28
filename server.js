9c0833a102f48aca52ed1dc5ad6c42602fdbd7f5
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-01-28 16:18:27 +0530

    moved response to http server

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const http = require('http');
 const { processRequest } = require('./lib/processRequest');
 
 const handleConnection = function(req, res) {
-  const responseText = processRequest(req);
-  res.end(responseText.body);
+  req.setEncoding('utf8');
+  let data = '';
+  req.on('data', chunk => {
+    console.log(chunk);
+    console.log(data);
+    data = data + chunk;
+  });
+
+  req.on('end', () => {
+    const response = processRequest(req, data);
+    res.setHeader('Content-Type', response.contentType);
+    res.setHeader('Status-Code', response.statusCode);
+    res.end(response.content);
+  });
 };
 
 const main = function() {

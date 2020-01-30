5bf12a49beb296ecf966cfc9b336a03f751d5a9e
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-01-30 10:08:51 +0530

    added appClass and eslint

--- server.js
+++ server.js
@@ -1,25 +1,11 @@
 const http = require('http');
-const { handleRequest } = require('./lib/handlers');
-
-const handleConnection = function(req, res) {
-  req.setEncoding('utf8');
-  let data = '';
-  req.on('data', chunk => (data += chunk));
-
-  req.on('end', () => {
-    const response = handleRequest(req, data);
-    res.writeHead(response.statusCode, {
-      'Content-Type': response.contentType
-    });
-    res.end(response.content);
-  });
-};
+const { app } = require('./lib/handlers');
 
 const main = function() {
-  const server = new http.Server(handleConnection);
-  server.listen(2300, () => {
-    console.log('started listening on port :', server.address());
-  });
+  const server = new http.Server(app.serve.bind(app));
+  server.listen(4000, () =>
+    console.log('started listening on ', server.address())
+  );
 };
 
 main();

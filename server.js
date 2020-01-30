c04e12b7d059d41992e8cc04114650708ea244ac
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-01-30 10:57:55 +0530

    removed eslint errors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const http = require('http');
+const { stdout } = require('process');
 const { app } = require('./lib/handlers');
 
 const main = function() {
   const server = new http.Server(app.serve.bind(app));
-  server.listen(4000, () =>
-    console.log('started listening on ', server.address())
+  const DEFAULT_PORT = 4000;
+  server.listen(DEFAULT_PORT, () =>
+    stdout.write(`started listening on ${DEFAULT_PORT}`)
   );
 };
 

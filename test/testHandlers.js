51909e02b353a2aeeb4aed2b338c19ca6ce626e6
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-01-30 21:43:49 +0530

    added tests for post method and bad file

--- test/testHandlers.js
+++ test/testHandlers.js
@@ -12,6 +12,26 @@ describe('GET /', () => {
       .expect(200, done);
   });
 
+  it('should return Ageratum', done => {
+    const expectedHeading = new RegExp(' Ageratum</header>');
+    request(app.serve.bind(app))
+      .get('/Ageratum.html')
+      .expect('Content-Length', '1338')
+      .expect('Content-Type', 'text/html')
+      .expect(expectedHeading)
+      .expect(200, done);
+  });
+
+  it('should return Ageratum', done => {
+    const expectedHeading = new RegExp(' Abeliophyllum</header>');
+    request(app.serve.bind(app))
+      .get('/Abeliophyllum.html')
+      .expect('Content-Length', '1569')
+      .expect('Content-Type', 'text/html')
+      .expect(expectedHeading)
+      .expect(200, done);
+  });
+
   it('should return guestBook.html', done => {
     const expectedHeading = new RegExp('<h1>Leave a comment</h1>');
     request(app.serve.bind(app))
@@ -26,5 +46,21 @@ describe('GET /', () => {
       .get('/bad')
       .expect('Content-Type', 'text/html')
       .expect(404, done);
+  });
+});
+
+describe('POST /', () => {
+  it('should run post method on /guestBook.html', done => {
+    request(app.serve.bind(app))
+      .post('/guestBook.html')
+      .send('username=anuja&comment=heyy')
+      .expect(302, done);
+  });
+
+  it('should give method not found for post on index.html', done => {
+    request(app.serve.bind(app))
+      .post('/index.html')
+      .send('username=anuja&comment=heyy')
+      .expect(405, done);
   });
 });
